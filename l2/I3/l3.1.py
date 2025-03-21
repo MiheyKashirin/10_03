7a70d629bbd4f59d53f6db3367ffd95b573a55ff
User <mihey.kashirin@icloud.com>
2025-03-21 12:53:52 +0200

    true commit

--- l2/I3/l3.1.py
+++ l2/I3/l3.1.py
@@ -1,25 +1,25 @@
 import math
-def calculator():
-    number1=float(input("Type number1:"))
-    operation=input("Type + - * /:")
-    number2=float(input("Type number2:"))
-    if operation == "/":
-        if number2 == 0:
-            print("Error")
-            return
-    if operation == "+":
-        result=number1+number2
-    elif operation=="-":
-        result=number1-number2
-    elif operation=="*":
-        result=number1*number2
-    elif operation=="/":
-        result=number1/number2
-    else:
-        print("Error")
-        return
-    print(f"{result}")
-calculator()
+number1 = float(input("Type number1: "))
+operation = input("Type + - * /: ")
+number2 = float(input("Type number2: "))
+if operation == "/":
+    if number2 == 0:
+        print("Error: Division by zero!")
+        exit()
+if operation == "+":
+    result = number1 + number2
+elif operation == "-":
+    result = number1 - number2
+elif operation == "*":
+    result = number1 * number2
+elif operation == "/":
+    result = number1 / number2
+else:
+    print("Error: Invalid operation!")
+    exit()
+print(f"Result: {result}")
+
+
 
 
 
