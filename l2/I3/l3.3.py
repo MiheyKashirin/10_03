8df79365d942111cadb4e1c63300c40f7d761df7
User <mihey.kashirin@icloud.com>
2025-03-25 21:59:48 +0200

    2 commit

--- l2/I3/l3.3.py
+++ l2/I3/l3.3.py
@@ -1,12 +1,7 @@
-my_list=[1,2,3,4,5,6]
-def split_list(lst):
-    if len(lst)==0:
-        return [[],[]]
-    mid=(len(lst) + 1) // 2
-    return [lst[:mid],lst[mid:]]
-print(split_list(my_list))
-print(split_list([1, 2, 3, 4, 5, 6]))
-print(split_list([1, 2, 3]))
-print(split_list([1, 2, 3, 4, 5]))
-print(split_list([1]))
-print(split_list([]))
+my_list = [1,2,3,4,5,6]
+if len(my_list) == 0:
+    result = [[], []]
+else:
+    mid = (len(my_list) + 1) // 2
+    result = [my_list[:mid], my_list[mid:]]
+print(result)
