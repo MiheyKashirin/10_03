2af95506730aa6a10ecc41293e61d6103673658d
User <mihey.kashirin@icloud.com>
2025-04-03 12:39:43 +0300

    1 commit

--- l2/I3/l4/l4.3.py
+++ l2/I3/l4/l4.3.py
@@ -2,10 +2,7 @@ import random
 my_list=[random.randint(1,10) for i in range(random.randint(3,10))]
 print(my_list)
 my_list2=[]
-if len(my_list)>=1:
-    my_list2.append(my_list[0])
-if len(my_list)>=3:
-    my_list2.append(my_list[2])
-if len(my_list)>=2:
-    my_list2.append(my_list[-2])
+my_list2.append(my_list[0])
+my_list2.append(my_list[2])
+my_list2.append(my_list[-2])
 print(my_list2)
