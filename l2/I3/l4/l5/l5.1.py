68a30b82e7342c5fcfcadd592c063e02cc9fd1ee
User <mihey.kashirin@icloud.com>
2025-04-04 18:11:45 +0300

    2  commit

--- l2/I3/l4/l5/l5.1.py
+++ l2/I3/l4/l5/l5.1.py
@@ -4,8 +4,6 @@ is_valid = (
     name not in keyword.kwlist and
     not name[0].isdigit() and
     all(c.islower() or c.isdigit() or c == '_' for c in name) and
-    not name.startswith('_') and
-    not name.endswith('_') and
     '__' not in name
 )
 if name == "_":
