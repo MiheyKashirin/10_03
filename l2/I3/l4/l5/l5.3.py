45a7b6a2ad34955b1d4451109b51004fd478d0db
User <mihey.kashirin@icloud.com>
2025-04-05 19:18:48 +0300

    2 commit

--- l2/I3/l4/l5/l5.3.py
+++ l2/I3/l4/l5/l5.3.py
@@ -1,8 +1,8 @@
 import string
 text = 'Should, I. subscribe? Yes!'
 words = text.split()
-cleaned_words = [''.join(c for c in word if c not in string.punctuation) for word in words]
-hashtag = ''.join(word.capitalize() for word in cleaned_words)
+text_words = [''.join(c for c in word if c not in string.punctuation) for word in words]
+hashtag = ''.join(word.capitalize() for word in text_words)
 if len(hashtag) > 140:
     hashtag = hashtag[:140]
 hashtag = '#' + hashtag
